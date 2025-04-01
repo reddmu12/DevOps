81d032fa6f3ee195ec18a29b72eb54e282561735
reddmu12 <116534982+reddmu12@users.noreply.github.com>
2025-04-01 12:17:17 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,1 +1,15 @@
-systemctl status jenkins
+pipeline {
+    agent any
+    stages {
+        stage('Check Jenkins Service Status') {
+            steps {
+                script {
+                    def status = sh(script: 'systemctl status jenkins', returnStatus: true)
+                    if (status != 0) {
+                        error("Jenkins service is not running!")
+                    }
+                }
+            }
+        }
+    }
+}
